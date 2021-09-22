3843a6615a396062af04f8450261031e64e63135
S. Morad <sm2558@cam.ac.uk>
2021-09-22 06:57:19 +0000

    Learned temporal edges

--- src/gcm/edge_selectors/temporal.py
+++ src/gcm/edge_selectors/temporal.py
@@ -1,5 +1,6 @@
 import torch
 from typing import List
+from gcm.util import diff_or, Spardmax
 
 # VISDOM TOP HALF PROVIDES BEST PERF
 
@@ -16,7 +17,7 @@ from typing import List
 class TemporalBackedge(torch.nn.Module):
     """Add temporal directional back edge, e.g., node_{t} -> node_{t-1}"""
 
-    def __init__(self, hops: List[int] = [1], direction="forward"):
+    def __init__(self, hops: List[int] = [1], direction="forward", learned=False, learning_window=10, deterministic=False, num_samples=3):
         """
         Hops: number of hops in the past to connect to
         E.g. [1] is t <- t-1, [2] is t <- t-2,
@@ -31,10 +32,34 @@ class TemporalBackedge(torch.nn.Module):
         self.hops = hops
         assert direction in ["forward", "backward", "both"]
         self.direction = direction
+        self.learned = learned
+        if learned:
+            self.window = torch.nn.Parameter(torch.ones(learning_window))
+            self.num_samples = num_samples
+            self.deterministic = deterministic
+            if deterministic:
+                self.spardmax = Spardmax()
 
-    def forward(self, nodes, adj_mats, edge_weights, num_nodes, B):
-        # TODO: Fix this to work with multiple hops
-        # assert self.hops == [1], "num_hops >1 not working yet"
+    def learned_forward(self, nodes, adj_mats, edge_weights, num_nodes, B):
+        if self.window.device != nodes.device:
+            self.window = self.window.to(nodes.device)
+        for b in range(B):
+            if num_nodes[b] == 0:
+                continue
+            relative_window = self.window[:num_nodes[b]]
+            if self.deterministic:
+                mask = self.spardmax(
+                    relative_window.reshape(1, -1) 
+                ).reshape(-1)
+            else:
+                masks = []
+                for i in range(self.num_samples):
+                    masks.append(torch.nn.functional.gumbel_softmax(relative_window, hard=True))
+                mask = diff_or(masks)
+            adj_mats[b][num_nodes[b]][:num_nodes[b]] = adj_mats[b][num_nodes[b]][:num_nodes[b]] + mask
+        return adj_mats, edge_weights
+
+    def deterministic_forward(self, nodes, adj_mats, edge_weights, num_nodes, B):
         for hop in self.hops:
             [valid_batches] = torch.where(num_nodes >= hop)
             if self.direction in ["forward", "both"]:
@@ -51,3 +76,10 @@ class TemporalBackedge(torch.nn.Module):
                 ] = 1
 
         return adj_mats, edge_weights
+        
+
+    def forward(self, nodes, adj_mats, edge_weights, num_nodes, B):
+        if self.learned:
+            return self.learned_forward(nodes, adj_mats, edge_weights, num_nodes, B)
+        
+        return self.deterministic_forward(nodes, adj_mats, edge_weights, num_nodes, B)
