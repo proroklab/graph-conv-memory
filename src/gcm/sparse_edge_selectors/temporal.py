1cb6f70212f8a2647ecc0ddc251dd50718d13b44
S. Morad <sm2558@cam.ac.uk>
2021-11-08 14:58:02 +0000

    Fix temporal sparse edge

--- src/gcm/sparse_edge_selectors/temporal.py
+++ src/gcm/sparse_edge_selectors/temporal.py
@@ -29,31 +29,42 @@ class TemporalEdge(torch.nn.Module):
 
         # TODO: also get rid of invalid (-1, -2, ...) edges
         batch_offsets = util.get_batch_offsets(T, taus)
-        
-        edge_base = [
-            torch.arange(
-                T[b], T[b] + taus[b], 
-                device=nodes.device
-            ) 
-            for b in range(B)  
-        ]
+        edge_base = []
+        edge_base_offsets = []
+
+        # Build a base of edges (x - hop for all hops)
+        # then we add the batch offsets to them
+        for b in range(B):
+            edge_base.append(
+                torch.arange(
+                    T[b], T[b] + taus [b], device=nodes.device
+                ) 
+            )
+
+            edge_base_offsets.append(
+                batch_offsets[b] * torch.ones(
+                    taus[b], device=nodes.device, dtype=torch.long
+                ) 
+            )
 
         # No edges to add
         if len(edge_base) < 1:
             empty_edges = torch.empty((2,0), device=nodes.device, dtype=torch.long)
-            empty_weights = torch.empty((0.0), device=nodes.device, dtype=torch.float)
+            empty_weights = torch.empty((0), device=nodes.device, dtype=torch.float)
             return empty_edges, empty_weights
 
         edge_base = torch.cat(edge_base)
-        # shape [B, t + tau, hops]
+        edge_base_offsets = torch.cat(edge_base_offsets)
         edge_ends = edge_base.unsqueeze(-1).repeat(1, len(self.hops)) 
-        # shape [B, t + tau, hops]
-        # Remove invalid edges (<0)
 
         edge_starts = edge_ends - self.hops.to(nodes.device)
         edge_starts = edge_starts.flatten()
         edge_ends = edge_ends.flatten()
+        # Remove invalid edges (<0) before we add offsets
         mask = (edge_starts >= 0)
+        # Offset edges
+        edge_starts = edge_starts + edge_base_offsets.repeat_interleave(len(self.hops))
+        edge_ends = edge_ends + edge_base_offsets.repeat_interleave(len(self.hops))
 
         new_edges = torch.stack((edge_starts, edge_ends))
         new_weights = torch.ones_like(new_edges[0], dtype=torch.float)
@@ -61,5 +72,7 @@ class TemporalEdge(torch.nn.Module):
         # Filter invalid edges
         new_edges = new_edges[:, mask]
         new_weights = new_weights[mask]
+
+        assert torch.all(new_edges[0] < new_edges[1]), "Tried to add invalid edge"
 
         return new_edges, new_weights
