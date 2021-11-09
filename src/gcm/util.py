5e86dcedfeb52659558f39af12b2b1d0dbae048b
Steven Morad <sm2558@cam.ac.uk>
2021-11-09 20:40:52 +0000

    Cleanup SparsegenLin stuff from util

--- src/gcm/util.py
+++ src/gcm/util.py
@@ -56,56 +56,6 @@ class Hardmax(torch.nn.Module):
         return y_hard - y_soft.detach() + y_soft
 
 
-class SparsegenLin(torch.nn.Module):
-    def __init__(self, lam, normalized=True):
-        super().__init__()
-        self.lam = lam
-        self.normalized = normalized
-
-    def forward(self, z):
-        bs = z.data.size()[0]
-        dim = z.data.size()[1]
-
-        # sort z
-        z_sorted = torch.sort(z, descending=True)[0]
-
-        # calculate k(z)
-        z_cumsum = torch.cumsum(z_sorted, dim=1)
-        k = torch.autograd.Variable(
-            torch.arange(1, dim + 1, device=z.device).unsqueeze(0).repeat(bs, 1)
-        )
-        z_check = torch.gt(1 - self.lam + k * z_sorted, z_cumsum)
-
-        # because the z_check vector is always [1,1,...1,0,0,...0] finding the
-        # (index + 1) of the last `1` is the same as just summing the number of 1.
-        k_z = torch.sum(z_check.float(), 1)
-
-        # calculate tau(z)
-        tausum = torch.sum(z_check.float() * z_sorted, 1)
-        tau_z = (tausum - 1 + self.lam) / k_z
-        prob = z.sub(tau_z.view(bs, 1).repeat(1, dim)).clamp(min=0)
-        if self.normalized:
-            prob /= 1 - self.lam
-        return prob
-
-
-class Spardgen(torch.nn.Module):
-    """A hard version of sparsegen-lin"""
-
-    def __init__(self, dim=-1, lam=0.75):
-        super().__init__()
-        self.dim = dim
-        self.sm = SparsegenLin(lam)
-
-    def forward(self, x):
-        # Only takes up to 2ds so reshape
-        x_in = x.reshape(-1, x.shape[-1])
-        # Straight through.
-        y_soft = self.sm(x_in).reshape(x.shape)
-        y_hard = (y_soft != 0).float()
-        return y_hard - y_soft.detach() + y_soft
-
-
 @torch.jit.script
 def get_nonpadded_idxs(T: torch.Tensor, taus: torch.Tensor, B: int):
     """Get the non-padded indices of a zero-padded
