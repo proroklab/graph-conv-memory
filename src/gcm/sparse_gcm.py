Fix enormous bug -- sparse GCM gradients were not propagating thru adj correctly

src/gcm/sparse_gcm.py
@@ -103,6 +103,8 @@ class SparseGCM(torch.nn.Module):
             hidden = self.get_initial_hidden_state(x)
 
         nodes, adj, T = hidden
+        # TODO remove coalesce when bug is fixed
+        adj = adj.coalesce()
 
         N = nodes.shape[1]
         B = x.shape[0]
@@ -126,14 +128,15 @@ class SparseGCM(torch.nn.Module):
         dirty_nodes = nodes.clone()
 
         if self.edge_selectors:
-            new_adj = self.edge_selectors(dirty_nodes, T, taus, B)
-            new_idx = torch.cat([adj._indices(), new_adj._indices()], dim=-1) 
-            new_val = torch.cat([adj._values(), new_adj._values()], dim=-1)
+            # TODO remove coalesce when bug is fixed
+            new_adj = self.edge_selectors(dirty_nodes, T, taus, B).coalesce()
+            new_idx = torch.cat([adj.indices(), new_adj.indices()], dim=-1) 
+            new_val = torch.cat([adj.values(), new_adj.values()], dim=-1)
             adj = torch.sparse_coo_tensor(
                 indices=new_idx, 
                 values=new_val, 
                 size=adj.shape
-            )
+            ).coalesce()
 
 
         # Thru network
@@ -142,16 +145,18 @@ class SparseGCM(torch.nn.Module):
         if self.positional_encoder:
             dirty_nodes = self.positional_encoder(dirty_nodes, T + taus)
         if self.aux_edge_selectors:
-            new_adj = self.aux_edge_selectors(dirty_nodes, T, taus, B)
-            new_idx = torch.cat([adj._indices(), new_adj._indices()], dim=-1) 
-            new_val = torch.cat([adj._values(), new_adj._values()], dim=-1)
-            adj = torch.sparse_coo_tensor(indices=new_idx, values=new_val, size=adj.shape)
+            # TODO remove coalesce when bug is fixed
+            new_adj = self.aux_edge_selectors(dirty_nodes, T, taus, B).coalesce()
+            new_idx = torch.cat([adj.indices(), new_adj.indices()], dim=-1) 
+            new_val = torch.cat([adj.values(), new_adj.values()], dim=-1)
+            adj = torch.sparse_coo_tensor(indices=new_idx, values=new_val, size=adj.shape).coalesce()
 
         # Remove duplicates from edge selectors
         # and set all weights to 1.0 without cancelling out gradients
         # from logits.
         # For some reason, torch_geometric coalesces incorrectly here
-        adj = adj.coalesce()
+        # TODO: remove comment when coalesce bug is fixed
+        # adj = adj.coalesce()
         adj = torch.sparse_coo_tensor(
             indices=adj.indices(),
             values=adj.values() / adj.values().detach(),

src/gcm/util.py
@@ -92,14 +92,17 @@ def sparse_gumbel_softmax(
     tau: float=1, 
     hard: bool=False,
     ) -> torch.sparse_coo:
-    gumbels = -torch.empty_like(logits._values()).exponential_().log()
-    gumbels = (logits._values() + gumbels) / tau
+    # TODO remove coalesce when bug is fixed
+    logits = logits.coalesce()
+    gumbels = -torch.empty_like(logits.values()).exponential_().log()
+    gumbels = (logits.values() + gumbels) / tau
     gumbels = torch.sparse_coo_tensor(
-        indices=logits._indices(),
-        values=logits._values(),
+        indices=logits.indices(),
+        values=gumbels,
         size=logits.shape
     )
-    y_soft = torch.sparse.softmax(gumbels, dim=dim)
+    # TODO remove coalesce when bug is fixed
+    y_soft = torch.sparse.softmax(gumbels, dim=dim).coalesce()
 
     if not hard:
         return y_soft

tests/test_sparse_gcm.py
@@ -752,6 +752,21 @@ class TestLearnedEdge(unittest.TestCase):
         if not torch.all(hidden[1].coalesce().indices() == desired):
             self.fail(f"{hidden[1].coalesce().indices()} != {desired}")
 
+    def test_grad(self):
+        B = 2
+        gsize = 4
+        taus = gsize * torch.ones(B, dtype=torch.long)
+        T = torch.zeros(B, dtype=torch.long)
+        obs = torch.zeros(B, gsize, self.F)
+        canary = torch.tensor([1.0], requires_grad=True)
+        obs = obs * canary
+
+
+        sel = SLearnedEdge(input_size=self.F, num_edge_samples=10, window=16)
+        adj = sel(obs, T, taus, B)
+        adj.coalesce().values().sum().backward()
+        self.assertTrue(canary.grad is not None)
+
 
 
 class TestUtil(unittest.TestCase):
@@ -851,7 +866,8 @@ class TestE2E(unittest.TestCase):
             sparse_g, graph_size=num_obs, edge_selectors=SLearnedEdge(obs_size),
             max_hops=2
         )
-        obs = torch.rand(B, num_obs, obs_size, requires_grad=True)
+        canary = torch.tensor([1.0], requires_grad=True)
+        obs = torch.rand(B, num_obs, obs_size) * canary 
         taus = torch.ones(B, dtype=torch.long)
         hidden = None
         with torch.no_grad():
@@ -865,7 +881,8 @@ class TestE2E(unittest.TestCase):
         tmp = util.pack_hidden(hidden, B, max_edges = 5 * num_obs)
         tmp = util.unpack_hidden(tmp, B)
         out.mean().backward()
-        self.assertTrue(obs.grad is not None)
+        self.assertTrue(canary.grad is not None)
+
 
 
 if __name__ == "__main__":